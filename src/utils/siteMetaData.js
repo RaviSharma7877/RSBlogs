a702fa1b8bdddb66deece2f4d6c5560324f7d73a
RaviSharma7877 <sraa7877@gmail.com>
2024-05-08 19:32:52 +0530

    done

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,20 @@
+// please update required information
+const siteMetadata = {
+    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
+    author: 'RSBlogs',
+    headerTitle: 'Next.js Blog',
+    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: 'sraa7877@gmail.com', 
+    github: 'https://github.com/RaviSharma7877',
+    youtube: 'https://youtube.com/codebucks',
+    linkedin: 'https://www.linkedin.com/in/ravi-sharma7877/',
+    dribbble: 'https://www.dribbble.com',
+    locale: 'en-US',
+  }
+  
+  module.exports = siteMetadata
